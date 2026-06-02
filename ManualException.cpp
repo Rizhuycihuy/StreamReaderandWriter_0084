83f42b6e5ca04f30a6cacbdeb09a583919ee393c
Rizhuycihuy <rizki003131@gmail.com>
2026-06-02 17:09:44 +0700

    membuat isi dari try untuk pengujian jika  eror

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    try
+    {
+        cout << "Selamat belajar di prodi ti umy" << endl;
+        //throw 0.5;
+        cout << " pernyataan tidak akan di tampilkan" << endl;
+    }
+}
